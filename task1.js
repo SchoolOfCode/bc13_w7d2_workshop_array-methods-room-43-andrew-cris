0f33f84140eeaead2d9809e719ae3553e2f5de4c
Cristhyan <cristhyancalderon@hotmail.co.uk>
2022-11-08 13:49:34 +0000

    task 2 Done

--- task1.js
+++ task1.js
@@ -1,3 +1,13 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(function(cats) {
+    let firstLetter = (Array.from(cats)[0])
+    let upperCase = firstLetter.toUpperCase()
+    let trimmed = cats.substring(1)
+    return upperCase + trimmed
+    
+})
+console.log(capitalisedCats)
+//take in cats 
+//turn each item into uppercase
+//using .map method to run through a function that generates a new array
--- task2.js
+++ task2.js
@@ -8,4 +8,7 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter(function(animals){
+  return animals.charAt(0) === "b";
+})
+console.log(bListAnimals)
